39b4c802e7c3730bb6b50d1e55bf00870543c0f1
radkin <noeldamonmiller@gmail.com>
2020-08-24 16:37:53 -0700

    skyline_products added api endpoints and model
    we have nearly everything except the data for our products. This nearly
    sets us up for loading it. Possible there are problems with the schema
    or handling of these data

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'api/v1/products#index'
+
+  resources :products
+
+  get 'api/v1/products' => 'api/v1/products/index'
 end
