b029b139b54a477cfb3c34a1b356c5b96fa28807
radkin <noeldamonmiller@gmail.com>
2020-08-26 10:34:54 -0700

    api_version
    started over with mysql and api which should leave out all the other
    stuff we don't need

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  root 'api/v1/products#index'
-
-  resources :products
-
-  get 'api/v1/products' => 'api/v1/products/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
